ffa558f8ed643767b75f4f1349c840a71131920e
Mike Robinson <mike@studiolift.com>
2012-08-16 16:08:06 +0100

    Initial commit - working version

---
+++ README.md
@@ -0,0 +1,21 @@
+# Stars for Basecamp Classic
+
+Adds "important" stars to Basecamp Classic todos if they contain an asterisk (*)
+
+## Installation
+
+Open the Userscript and you will be prompted to install the extension.
+
+## Usage
+
+If a todo's text contains an asterisk (*) it will remove the marker and place an orange star. My preference is to prefix the todo text, e.g. "* My super important task"
+
+It also has legacy support for [Enhance!](https://github.com/studiolift/Enhance-for-Basecamp) HOT/WARM syntax (HOT will appear slightly darker).
+
+## Contributing
+
+1. Fork it
+2. Create your feature branch (`git checkout -b my-new-feature`)
+3. Commit your changes (`git commit -am 'Added some feature'`)
+4. Push to the branch (`git push origin my-new-feature`)
+5. Create new Pull Request
---
+++ Stars_for_Basecamp.user.js
@@ -0,0 +1,44 @@
+// ==UserScript==
+// @name        Stars for Basecamp Classic
+// @description Adds "important" stars to Basecamp Classic todos if they contain an asterisk (*)
+// @match       https://*.basecamphq.com/*
+// @include     https://*.basecamphq.com/*
+// @version     0.1
+// @author      Mike Robinson
+// @homepage    http://twitter.com/akamike
+// ==/UserScript==
+(function(){
+  var doc = document,
+      body = doc.getElementsByTagName('body')[0],
+      style = doc.createElement('style');
+
+  // Add custom styles
+  style.textContent = [
+    '.todo_star { color:#fff; display:inline-block; font-size:3em; line-height:8%; margin-right:3px; background-color:orange; font-family:Helvetica,Arial; padding:0.46em 0.1em 0; vertical-align:middle; }',
+    '.hot { background-color:darkOrange; }'
+  ].join('\n');
+
+  doc.getElementsByTagName('head')[0].appendChild(style);
+
+  // Format markers
+  var todos = body.querySelectorAll('.todolist .content, .item .item_content, .items_wrapper .content span, .page_header .content .item, .event .item span');
+
+  for (var i = 0; i < todos.length; i++) {
+    var todo = todos[i], t;
+
+    // matches [HOT] and [WARM] for legacy Enhance! tasks
+    if (t = todo.textContent.match(/(\[HOT\]|\[WARM\]|\*)/g)) {
+      var p = doc.createElement('span');
+          p.textContent = "*";
+          p.className = 'todo_star';
+
+      // Subtle difference
+      if (t[0] == '[HOT]') {
+        p.className += ' hot';
+      }
+
+      todo.innerHTML = todo.innerHTML.replace(t[0], '');
+      todo.insertBefore(p, todo.firstChild);
+    }
+  }
+})();
